8d2986f6b585abbd7fe268dc5fd7bd3edcc75412
Jeong Byeong Ho <zero8660@gmail.com>
2024-09-18 23:46:47 +0900

    redux-toolkit 설치 및 프로바이더, 스토어 설정.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "../app/styles/globals.scss";
+import Providers from "./component/Providers";
 
 export const metadata: Metadata = {
   title: "Square Box",
@@ -13,7 +14,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
