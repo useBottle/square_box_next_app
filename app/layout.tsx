6655cb8e044757df7c02b2484579647870706597
Jeong Byeong Ho <zero8660@gmail.com>
2024-09-19 00:07:52 +0900

    폰트 설정.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,18 @@
 import type { Metadata } from "next";
 import "../app/styles/globals.scss";
 import Providers from "./component/Providers";
+import { Noto_Sans_KR, Prompt } from "next/font/google";
+
+const notoSansKR = Noto_Sans_KR({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+const prompt = Prompt({
+  subsets: ["latin"],
+  display: "swap",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
 
 export const metadata: Metadata = {
   title: "Square Box",
@@ -13,8 +25,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>
+    <html lang="kr">
+      <body className={notoSansKR.className}>
         <Providers>{children}</Providers>
       </body>
     </html>
