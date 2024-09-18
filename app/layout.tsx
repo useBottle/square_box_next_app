f4f272769c3006ec5be97ec07fff97c4d23df63f
Jeong Byeong Ho <zero8660@gmail.com>
2024-09-19 00:38:55 +0900

    파비콘 설정.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import "../app/styles/globals.scss";
+import "../styles/globals.scss";
 import Providers from "./component/Providers";
 import { Noto_Sans_KR, Prompt } from "next/font/google";
 
@@ -17,6 +17,13 @@ const prompt = Prompt({
 export const metadata: Metadata = {
   title: "Square Box",
   description: "Square Box app with Next.js",
+  icons: {
+    apple: "/icon-192.png",
+    other: {
+      rel: "icon",
+      url: "/favicon.png",
+    },
+  },
 };
 
 export default function RootLayout({
