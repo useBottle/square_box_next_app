16e341728ca7d82e53dd3f886665461d0870ebe5
Jeong Byeong Ho <zero8660@gmail.com>
2024-09-20 18:34:56 +0900

    모바일 메뉴 버튼 스타일링.

--- app/component/MobileNav.tsx
+++ app/component/MobileNav.tsx
@@ -7,10 +7,36 @@ import { css } from "@emotion/react";
 import { useDispatch, useSelector } from "react-redux";
 import { IoCloseOutline } from "react-icons/io5";
 import { setNavMenu } from "@/store/switches";
+import { FaHome, FaNewspaper, FaYoutube, FaBookmark } from "react-icons/fa";
 
 export default function MobileNav(): JSX.Element {
   const dispatch = useDispatch<AppDispatch>();
   const { navMenu } = useSelector((state: RootState) => state.switches);
+
+  const menuList = css({
+    width: "20rem",
+    height: "5rem",
+    border: "1px solid #eee",
+    borderRadius: "5px",
+    background: "transparent",
+    margin: "1rem 0",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    fontSize: "1.4rem",
+  });
+
+  const menuIcon = css({
+    marginRight: "1rem",
+    color: "var(--main-color)",
+  });
+
+  const menuItems = [
+    { icon: <FaHome css={menuIcon} />, text: "Home" },
+    { icon: <FaNewspaper css={menuIcon} />, text: "News" },
+    { icon: <FaYoutube css={menuIcon} />, text: "Youtube" },
+    { icon: <FaBookmark css={menuIcon} />, text: "BookMark" },
+  ];
 
   return (
     <nav
@@ -31,10 +57,25 @@ export default function MobileNav(): JSX.Element {
         })}
         onClick={() => dispatch(setNavMenu(navMenu ? false : true))}
       />
-      <ul>
-        <li></li>
-        <li></li>
-        <li></li>
+      <ul
+        css={css({
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+          height: "100vh",
+        })}
+      >
+        {menuItems.map((item, index) => {
+          return (
+            <li key={index}>
+              <button css={menuList}>
+                {item.icon}
+                {item.text}
+              </button>
+            </li>
+          );
+        })}
       </ul>
     </nav>
   );
