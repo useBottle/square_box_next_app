64c4a3ff6291c0e225898b5196a974651a45dbc0
Jeong Byeong Ho <zero8660@gmail.com>
2024-09-27 20:35:44 +0900

    임시 로그인 input 필드 생성.

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -5,6 +5,10 @@ import { signIn } from "next-auth/react";
 export default function Signin() {
   return (
     <div>
+      <form>
+        <input placeholder="Email" />
+        <input placeholder="Password" />
+      </form>
       <button onClick={() => signIn("google")}>Sign in with Google</button>
       <button onClick={() => signIn("kakao")}>Sign in with Kakao</button>
     </div>
