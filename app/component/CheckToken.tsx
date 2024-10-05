068a66d83bde54edf7ff80ef53307d7d228c626b
Jeong Byeong Ho <zero8660@gmail.com>
2024-10-05 15:27:14 +0900

    클라이언트에서는 5분 마다 토큰 유효기간 체크. 유효기간 만료 5분 전이면 토큰 갱신 요청.

--- app/component/CheckToken.tsx
+++ app/component/CheckToken.tsx
@@ -1,36 +1,26 @@
 "use client";
 
-import { signIn, useSession } from "next-auth/react";
-import { useEffect, useState } from "react";
+import { useSession } from "next-auth/react";
+import { useEffect } from "react";
 
 export default function CheckToken() {
   const { data: session, update } = useSession();
 
-  // const [provider, setProvider] = useState<string>("");
-
   useEffect(() => {
     if (!session || !session.user || !session.expires) return;
 
-    // session.user.provider === "google" && setProvider("google");
-    // session.user.provider === "kakao" && setProvider("kakao");
-    // session.user.provider === "credentials" && setProvider("credentials");
-
     const tokenExpires = new Date(session.expires).getTime();
-    const refreshTime = tokenExpires - 5000;
-    console.log("tokenExpires: ", tokenExpires);
-    console.log("refreshTime: ", refreshTime);
-    console.log("Date.now time: ", Date.now());
-    console.log("UTC time: ", new Date().getTime());
+    const refreshTime = tokenExpires - 5 * 60 * 1000;
 
     const checkTokenExpires = async () => {
-      if (new Date().getTime() >= refreshTime) {
+      if (Date.now() >= refreshTime) {
         console.log("Refreshing access token");
-        // await signIn(provider);
         update();
       }
+      return;
     };
 
-    const intervalCheck = setInterval(checkTokenExpires, 1000);
+    const intervalCheck = setInterval(checkTokenExpires, 5 * 60 * 1000);
     return () => clearInterval(intervalCheck);
   }, [session]);
 
