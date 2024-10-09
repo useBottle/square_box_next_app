914ecd0a9ef3d1d728e52fab0bf211100b720488
Jeong Byeong Ho <zero8660@gmail.com>
2024-10-09 21:09:30 +0900

    socialRedirect 페이지 스타일 지정.

--- app/auth/social-redirect/page.tsx
+++ app/auth/social-redirect/page.tsx
@@ -1,13 +1,17 @@
+/** @jsxImportSource @emotion/react */
+
 "use client";
 
+import { container } from "@/styles/socialRedirect.styles";
 import { useRouter } from "next/navigation";
+import { css } from "@emotion/react";
 
 export default function SocialRedirect(): JSX.Element {
   const router = useRouter();
 
   return (
-    <div>
-      <div>소셜 계정이 이미 존재합니다.</div>
+    <div css={css(container)}>
+      <h1>소셜 계정이 이미 존재합니다</h1>
       <button onClick={() => router.push("/auth/signin")}>다시 로그인하기</button>
     </div>
   );
