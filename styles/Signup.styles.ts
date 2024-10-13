844b74cb620e244608a14048e8a1a0b4987796fd
Jeong Byeong Ho <zero8660@gmail.com>
2024-10-13 23:42:24 +0900

    회원가입 페이지 input 요소 스타일 적용.

--- styles/Signup.styles.ts
+++ styles/Signup.styles.ts
@@ -1,8 +1,13 @@
 import { CSSObject } from "@emotion/react";
+import { input } from "./default.styles";
 
 export const form: CSSObject = {
   display: "flex",
   justifyContent: "center",
   alignItems: "center",
   flexDirection: "column",
+
+  "& input": {
+    ...input,
+  },
 };
