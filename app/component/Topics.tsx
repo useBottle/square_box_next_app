a4a27c3f775eec09780b17b2d73986ac3d14797c
Jeong Byeong Ho <zero8660@gmail.com>
2024-10-27 23:46:00 +0900

    실시간 검색에 컴포넌트 생성.

---
+++ app/component/Topics.tsx
@@ -0,0 +1,13 @@
+/** @jsxImportSource @emotion/react */
+
+"use client";
+
+export default function Topics(): JSX.Element {
+  return (
+    <form>
+      <ul>
+        <li></li>
+      </ul>
+    </form>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import SearchBar from "./component/SearchBar";
+import Topics from "./component/Topics";
 
 export default function Home(): JSX.Element {
   return (
     <div>
       <SearchBar />
+      <Topics />
     </div>
   );
 }
