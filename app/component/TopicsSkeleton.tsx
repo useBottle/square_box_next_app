78cac2d29c7338018c9d5de9710b1c2c6d5ad397
Jeong Byeong Ho <zero8660@gmail.com>
2024-11-03 20:45:06 +0900

    Topics 의 스켈레톤 UI 스타일 객체를 Topics.styles.ts 로 분리.

--- app/component/TopicsSkeleton.tsx
+++ app/component/TopicsSkeleton.tsx
@@ -2,64 +2,14 @@
 
 "use client";
 
-import { css, CSSObject, keyframes } from "@emotion/react";
-
-const shine = keyframes({
-  from: {
-    transform: "skew(45deg) translateX(0%)",
-  },
-  to: {
-    transform: "skew(45deg) translateX(200%)",
-  },
-});
+import { skeleton } from "@/styles/Topics.styles";
+import { css } from "@emotion/react";
 
 export default function TopicsSkeleton(): JSX.Element {
   const arry = new Array(10).fill(null);
 
-  const styles: CSSObject = {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    width: "100%",
-
-    "& .list": {
-      width: "100%",
-      display: "grid",
-      gridTemplateColumns: "8% 84% 6%",
-      margin: "1rem 0",
-
-      "& span": {
-        position: "relative",
-        background: "lightgray",
-        height: "1.8rem",
-        borderRadius: "3px",
-        overflow: "hidden",
-
-        "&::before": {
-          content: '""',
-          position: "absolute",
-          top: 0,
-          left: 0,
-          width: "100%",
-          height: "100%",
-          background: "#ffffff33",
-          animation: `${shine} infinite 1.5s ease-in-out`,
-        },
-      },
-
-      "& .rank": {
-        marginLeft: "0.5rem",
-      },
-
-      "& .keyword": {
-        marginLeft: "2rem",
-        marginRight: "2rem",
-      },
-    },
-  };
-
   return (
-    <div css={css(styles)}>
+    <div css={css(skeleton)}>
       {arry.map((_, index) => {
         return (
           <div key={index} className="list">
--- styles/Topics.styles.ts
+++ styles/Topics.styles.ts
@@ -1,4 +1,4 @@
-import { CSSObject } from "@emotion/react";
+import { CSSObject, keyframes } from "@emotion/react";
 
 export const topicsForm: CSSObject = {
   display: "flex",
@@ -41,6 +41,57 @@ export const topicsForm: CSSObject = {
           color: "var(--state-down)",
         },
       },
+    },
+  },
+};
+
+const shine = keyframes({
+  from: {
+    transform: "skew(45deg) translateX(0%)",
+  },
+  to: {
+    transform: "skew(45deg) translateX(200%)",
+  },
+});
+
+export const skeleton: CSSObject = {
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  width: "100%",
+
+  "& .list": {
+    width: "100%",
+    display: "grid",
+    gridTemplateColumns: "8% 84% 6%",
+    margin: "1rem 0",
+
+    "& span": {
+      position: "relative",
+      background: "lightgray",
+      height: "1.8rem",
+      borderRadius: "3px",
+      overflow: "hidden",
+
+      "&::before": {
+        content: '""',
+        position: "absolute",
+        top: 0,
+        left: 0,
+        width: "100%",
+        height: "100%",
+        background: "#ffffff33",
+        animation: `${shine} infinite 1.5s ease-in-out`,
+      },
+    },
+
+    "& .rank": {
+      marginLeft: "0.5rem",
+    },
+
+    "& .keyword": {
+      marginLeft: "2rem",
+      marginRight: "2rem",
     },
   },
 };
