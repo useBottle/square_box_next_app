b41306a62988d945414f18d3ba8d6e201b225430
Jeong Byeong Ho <zero8660@gmail.com>
2024-12-12 23:06:59 +0900

    다이나믹 라우트 페이지에서 params 에 따라 데이터 인덱싱.

--- app/news/[id]/page.tsx
+++ app/news/[id]/page.tsx
@@ -2,6 +2,15 @@
 
 "use client";
 
+import { RootState } from "@/store/store";
+import { useParams } from "next/navigation";
+import { useSelector } from "react-redux";
+
 export default function NewsDynamic(): JSX.Element {
+  const news = useSelector((state: RootState) => state.news);
+  const params = useParams();
+  const newsId = params.id;
+  console.log("News ID:", newsId);
+
   return <div>news dynamic route</div>;
 }
