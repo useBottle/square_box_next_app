dd3aba75f6c3a8046855126a8f785f5a03de8f3c
Jeong Byeong Ho <zero8660@gmail.com>
2024-12-12 23:18:26 +0900

    인덱싱한 뉴스 데이터를 화면에 출력.

--- app/news/[id]/page.tsx
+++ app/news/[id]/page.tsx
@@ -3,14 +3,27 @@
 "use client";
 
 import { RootState } from "@/store/store";
+import Image from "next/image";
 import { useParams } from "next/navigation";
 import { useSelector } from "react-redux";
 
 export default function NewsDynamic(): JSX.Element {
   const news = useSelector((state: RootState) => state.news);
   const params = useParams();
-  const newsId = params.id;
+  const newsId = Number(params.id);
   console.log("News ID:", newsId);
 
-  return <div>news dynamic route</div>;
+  if (typeof newsId !== "number" || newsId < 0 || newsId >= news.length) {
+    // 나중에 잘못된 페이지 접근 UI 추가하기.
+    return <div>Invalid news ID</div>;
+  }
+
+  return (
+    <div>
+      <Image src={news[newsId].image} alt="newsImg" width={100} height={100} />
+      <h1>{news[newsId].title}</h1>
+      <div>{news[newsId].date}</div>
+      <p>{news[newsId].text}</p>
+    </div>
+  );
 }
