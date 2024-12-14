2523149967c513f82a693618a1d75a300366e06d
Jeong Byeong Ho <zero8660@gmail.com>
2024-12-14 15:43:52 +0900

    news api 에서 주석 삭제.

--- app/api/news/route.ts
+++ app/api/news/route.ts
@@ -33,46 +33,6 @@ export async function POST(req: Request) {
       };
       newsData.push(news);
     });
-    /* const newsData = await Promise.all(
-      newsList.map(async (_, item) => {
-        const h3 = $(item).find(".board-list.h3.pc_only").text().trim();
-        const href = $(item).find(".board-list.h3.pc_only a").attr("href");
-        const date = $(item).find(".date.pc_only em").text();
-        const summary = $(item).find(".text.pc_only a").text().replace(/\n/g, "").trim();
-
-        if (href) {
-          try {
-            const response = await axios.get(href);
-            const subPage = cheerio.load(response.data);
-            const img = subPage(".img-box img").attr("src");
-            const alt = subPage(".img-box img").attr("alt");
-            const text = subPage(".editor-p")
-              .map((_, item) => subPage(item).text().trim())
-              .get();
-
-            return {
-              title: h3,
-              image: img ? img : "",
-              alt: alt ? alt : "",
-              date: date,
-              summary: summary,
-              text: text,
-            };
-          } catch (error) {
-            console.error("Error fetching subpage:", error);
-          }
-        }
-
-        return {
-          title: h3,
-          image: "",
-          alt: "",
-          date: date,
-          summary: summary,
-          text: "",
-        };
-      }),
-    ); */
     return NextResponse.json({ newsData: newsData });
   } catch (error) {
     console.error(error);
