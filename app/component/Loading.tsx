dfa3cecddf4d073f84fb603022069d62169d399a
Jeong Byeong Ho <zero8660@gmail.com>
2024-12-19 02:27:56 +0900

    Loading UI 컴포넌트 생성.

---
+++ app/component/Loading.tsx
@@ -0,0 +1,13 @@
+/**jsxImportSource @emotion/react */
+
+"use client";
+
+import { css } from "@emotion/react";
+
+export default function Loading() {
+  return (
+    <div>
+      <h1>데이터를 불러오는 중입니다</h1>
+    </div>
+  );
+}
