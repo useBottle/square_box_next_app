3ce63a6d27b4d754fde7560101d9ced90a49297a
Jeong Byeong Ho <zero8660@gmail.com>
2025-01-02 22:48:01 +0900

    유튜브 데이터 오류 시 안내 페이지 추가.

--- app/youtube/[id]/page.tsx
+++ app/youtube/[id]/page.tsx
@@ -3,7 +3,8 @@
 "use client";
 
 import { RootState } from "@/store/store";
-import { css } from "@emotion/react";
+import { css, CSSObject } from "@emotion/react";
+import Link from "next/link";
 import { useSearchParams } from "next/navigation";
 import { useSelector } from "react-redux";
 
@@ -14,9 +15,36 @@ export default function YoutubeDynamic(): JSX.Element {
   const videoId = searchParams.get("id");
   const index = searchParams.get("index");
 
+  const infoText: CSSObject = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "column",
+    height: "80vh",
+    fontSize: "1.6rem",
+
+    "& a": {
+      color: "var(--reverse-font)",
+      padding: "1rem 2rem",
+      background: "var(--basic-font)",
+      borderRadius: "3px",
+      marginTop: "8rem",
+    },
+  };
+
+  if (!youtubeList || youtubeList.items.length === 0) {
+    return (
+      <div css={css(infoText)}>
+        <p>데이터가 만료되었습니다</p>
+        <p>다시 시도해주세요</p>
+        <Link href="/">HOME</Link>
+      </div>
+    );
+  }
+
   return (
     <div>
-      <div>youtube dynamic page</div>
+      <div></div>
     </div>
   );
 }
