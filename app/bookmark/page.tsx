9dcf58cdd0913cf71f4dc762be5f38639d07e68d
Jeong Byeong Ho <zero8660@gmail.com>
2025-01-04 01:36:14 +0900

    bookmark 각 컨텐츠 제목 추가.

--- app/bookmark/page.tsx
+++ app/bookmark/page.tsx
@@ -1,6 +1,7 @@
 import { authOptions } from "@/util/authOptions";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
+import styles from "../../styles/Bookmark.module.scss";
 
 export default async function Bookmark() {
   const session = await getServerSession(authOptions);
@@ -9,5 +10,10 @@ export default async function Bookmark() {
     redirect("/auth/signin");
   }
 
-  return <p>Welcom, {session.user?.name}</p>;
+  return (
+    <div className={styles.bookmark}>
+      <h4>북마크한 뉴스 컨텐츠</h4>
+      <h4>북마크한 유튜브 컨텐츠</h4>
+    </div>
+  );
 }
