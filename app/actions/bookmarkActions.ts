088c78c69cd2bd6e5b6a2fd08fea7ad8cf1436bb
Jeong Byeong Ho <zero8660@gmail.com>
2025-01-04 16:38:09 +0900

    bookmarkActions 초기 형태 생성.

---
+++ app/actions/bookmarkActions.ts
@@ -0,0 +1,6 @@
+"use server";
+
+import { LatestNewsArticle } from "@/types/types";
+import axios from "axios";
+
+export async function setNewsBookmark(article: LatestNewsArticle, username: string) {}
