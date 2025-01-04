ef6d1d3b4fe9e19ae4b3ce471b131bd154a223e1
Jeong Byeong Ho <zero8660@gmail.com>
2025-01-04 15:55:41 +0900

    유튜브 라우트에서 전송하는 날짜 포맷 변경.

--- app/api/youtube/route.ts
+++ app/api/youtube/route.ts
@@ -19,7 +19,7 @@ export async function POST(req: Request) {
     const month = String(date.getMonth() + 1).padStart(2, "0");
     const day = String(date.getDate()).padStart(2, "0");
 
-    return `${year}-${month}-${day}`;
+    return `${year}.${month}.${day}`;
   }
 
   try {
