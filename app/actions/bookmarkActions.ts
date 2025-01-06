d45464d4f476a2ad9e90a965f62f01fe0b3f813b
Jeong Byeong Ho <zero8660@gmail.com>
2025-01-07 02:00:32 +0900

    북마크된 뉴스 및 유튜브 데이터 검색 및 반환 서버 액션 함수 정의.

--- app/actions/bookmarkActions.ts
+++ app/actions/bookmarkActions.ts
@@ -1,3 +1,55 @@
 "use server";
 
+import MarkedNews from "@/models/markedNews";
+import MarkedYoutube from "@/models/markedYoutube";
 import dbConnect from "@/util/database";
+
+// 뉴스 북마크 데이터 검색 및 반환
+export async function getMarkedNews(username: string) {
+  try {
+    await dbConnect();
+
+    const findBookmark = await MarkedNews.find({ username: username, category: "news" });
+
+    if (findBookmark) {
+      return {
+        exists: true,
+        data: findBookmark,
+      };
+    }
+
+    if (!findBookmark) {
+      return {
+        exists: false,
+        message: "bookmarked news not found",
+      };
+    }
+  } catch (error) {
+    console.error("get bookmarked news articles failed", error);
+  }
+}
+
+// 유튜브 북마크 데이터 검색 및 반환
+export async function getMarkedYoutube(username: string) {
+  try {
+    await dbConnect();
+
+    const findBookmark = await MarkedYoutube.find({ username: username, category: "youtube" });
+
+    if (findBookmark) {
+      return {
+        exists: true,
+        data: findBookmark,
+      };
+    }
+
+    if (!findBookmark) {
+      return {
+        exists: false,
+        message: "bookmarked youtube videos not found",
+      };
+    }
+  } catch (error) {
+    console.error("get bookmarked youtube videos failed", error);
+  }
+}
