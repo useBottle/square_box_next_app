e9ea9faa8a60f39a04ab45e301e8c12657b3d6e0
Jeong Byeong Ho <zero8660@gmail.com>
2025-01-08 21:41:07 +0900

    home 컴포넌트를 서버 컴포넌트로 전환 후 LatestNews 컴포넌트 추가하여 최신 뉴스 fetch 로직 이동.

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,76 +1,16 @@
-/** @jsxImportSource @emotion/react */
-
-"use client";
-
-import Link from "next/link";
 import Topics from "../component/Topics";
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { AppDispatch, RootState } from "@/store/store";
-import { getLatestNewsArticle, getLatestNewsList } from "../actions/latestNewsActions";
-import { setLatestArticleSet, setLatestNewsList } from "@/store/latestNews";
-import Image from "next/image";
-import { latestNews } from "@/styles/LatestNews.styles";
-import { css } from "@emotion/react";
-import LatestNewsSkeleton from "../component/LatestNewsSkeleton";
 import TopicsServerComponent from "../component/TopicsServerComponent";
+import LatestNews from "../component/LatestNews";
 
 export const dynamic = "force-dynamic";
 
 export default function Home(): JSX.Element {
-  const dispatch = useDispatch<AppDispatch>();
-  const latestNewsList = useSelector((state: RootState) => state.latestNews.latestNewsList);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const result = await getLatestNewsList();
-      dispatch(setLatestNewsList(result?.newsTop10List));
-
-      const articles = await getLatestNewsArticle(result?.urls as string[]);
-      dispatch(setLatestArticleSet(articles));
-    };
-
-    fetchData();
-
-    // 10분마다 최신 뉴스 리스트 갱신 요청.
-    const intervalFetch = setInterval(
-      () => {
-        fetchData();
-      },
-      1000 * 60 * 10,
-    );
-
-    return () => clearInterval(intervalFetch);
-  }, []);
-
   return (
     <div>
       <Topics>
         <TopicsServerComponent />
       </Topics>
-      <div css={css(latestNews)}>
-        <h4>최신 뉴스 Top 10</h4>
-        <ul>
-          {latestNewsList.length !== 0 ? (
-            latestNewsList.map((item, index) => {
-              return (
-                <Link href={`/latest-news/detail?title=${encodeURIComponent(item.title)}`} key={index}>
-                  <li>
-                    <Image src={item.prevImg} width={100} height={100} alt="newsImg" />
-                    <div className="textGroup">
-                      <h6>{item.title}</h6>
-                      <div className="date">{item.date}</div>
-                      <p>{item.summary}</p>
-                    </div>
-                  </li>
-                </Link>
-              );
-            })
-          ) : (
-            <LatestNewsSkeleton />
-          )}
-        </ul>
-      </div>
+      <LatestNews />
     </div>
   );
 }
--- app/component/TopicsServerComponent.tsx
+++ app/component/TopicsServerComponent.tsx
@@ -1,12 +1,18 @@
 import axios from "axios";
 
-export default async function TopicsServerComponent() {
-  let keywordsData;
+const fetchKeyword = async (): Promise<void> => {
   try {
     const response = await axios.get("/api/topics");
-    keywordsData = response.data.top10;
+    const keywordsData = response.data.top10;
+    return keywordsData;
   } catch (error) {
     console.error("Failed fetching keyword data.", error);
   }
+};
+
+export default async function TopicsServerComponent() {
+  console.log("hi");
+  // const keywordsData = await fetchKeyword();
+
   return <div>TopicsServerComponent</div>;
 }
