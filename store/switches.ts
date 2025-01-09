f0c23bf80f911643013e0ecb8365cd9157c5f9e0
Jeong Byeong Ho <zero8660@gmail.com>
2025-01-09 17:58:22 +0900

    searchBar 에서 검색한 inputValue 를 전역 상태로 저장.

--- store/switches.ts
+++ store/switches.ts
@@ -4,6 +4,7 @@ const initialState = {
   navMenu: false,
   signoutModal: false,
   pageState: "default",
+  inputValue: "",
 };
 
 export const switches = createSlice({
@@ -19,8 +20,11 @@ export const switches = createSlice({
     setPageState(state, action: PayloadAction<string>) {
       state.pageState = action.payload;
     },
+    setInputValue(state, action: PayloadAction<string>) {
+      state.inputValue = action.payload;
+    },
   },
 });
 
-export const { setNavMenu, setSignoutModal, setPageState } = switches.actions;
+export const { setNavMenu, setSignoutModal, setPageState, setInputValue } = switches.actions;
 export default switches.reducer;
