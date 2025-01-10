1bff12edd95613339cb939395d41e322e9c33bc3
Jeong Byeong Ho <zero8660@gmail.com>
2025-01-11 00:15:36 +0900

    news/topics 콘솔 출력 삭제.

--- app/news/topics/page.tsx
+++ app/news/topics/page.tsx
@@ -28,7 +28,6 @@ export default function NewsOfTopics(): JSX.Element {
   const router = useRouter();
 
   useEffect(() => {
-    console.log(newsListOfSingleTopic);
     window.scrollTo({ top: 0 });
     // 뉴스 페이지 접속 여부 체크 -> 검색 컴포넌트에서 검색 요청 토글 역할
     dispatch(setPageState("news"));
