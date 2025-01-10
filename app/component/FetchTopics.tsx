b4ddb831f20c4294104361181464d8135be1f40a
Jeong Byeong Ho <zero8660@gmail.com>
2025-01-10 15:05:37 +0900

    Topics 에서 FetchTopics 로 fetchKeyword 1차 이식.

--- app/component/FetchTopics.tsx
+++ app/component/FetchTopics.tsx
@@ -1,5 +1,82 @@
 "use client";
 
+import { topicsForm } from "@/styles/Topics.styles";
+import { articleData, newsList, newsListWithKeyword, TopicsProps, TopicsType } from "@/types/types";
+import { css } from "@emotion/react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { FaPlus, FaMinus, FaCaretUp, FaCaretDown } from "react-icons/fa6";
+import Link from "next/link";
+import { setArticles, setNewsList, setTotalArticles } from "@/store/news";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "@/store/store";
+import { useRouter } from "next/navigation";
+import { setInputValue } from "@/store/switches";
+
 export default function FetchTopics() {
+  const dispatch = useDispatch<AppDispatch>();
+
+  useEffect(() => {
+    /**
+     * fetchKeyword
+     * 실시간 검색어를 순회하여 각 검색어에 해당하는 뉴스 리스트 요청.
+     * 실시간 검색어를 순회하여 얻은 뉴스 리스트마다 중첩으로 순회하여 각 뉴스 기사 데이터 요청.
+     */
+    const fetchKeyword = async () => {
+      try {
+        const topicsResponse = await axios.get("/api/topics");
+        // 실시간 검색어 별로 순회하여 뉴스 리스트 요청 후 배열로 상태 업데이트.
+
+        // 모든 실시간 검색어 키워드에 해당하는 뉴스 리스트들의 각 뉴스 기사 데이터.
+        const articlesOfTopics: { keyword: string; articles: articleData[] }[] = [];
+
+        const results = await Promise.all(
+          topicsResponse.data.top10.map(async (item: TopicsType) => {
+            const response = await axios.post("/api/news", {
+              inputValue: item.keyword,
+              sort: "relation",
+            });
+            const data = {
+              keyword: item.keyword,
+              newsList: response.data.newsList,
+            };
+
+            // 뉴스 리스트를 순회하여 각 기사의 url 수집.
+            const urls: string[] = [];
+            response.data.newsList.forEach((item: newsList) => {
+              if (item.href !== "") {
+                urls.push(item.href);
+              }
+            });
+
+            // 키워드에 해당하는 뉴스 리스트의 각 뉴스 기사 데이터 요청.
+            const responseArticles = await axios.post("/api/articles", { urls: urls });
+            const articlesData = {
+              keyword: item.keyword,
+              articles: responseArticles.data.articlesData,
+            };
+
+            // 위의 뉴스 리스트의 각 뉴스 기사 데이터 (articles) 를 articlesOfTopics 배열에 푸쉬.
+            articlesOfTopics.push(articlesData);
+
+            return data;
+          }),
+        );
+        // setNewsListArray(results); -> 슬라이스 생성 후 디스패치하기
+        // setTopics(topicsResponse.data.top10); -> 슬라이스 생성 후 디스패치하기
+        dispatch(setTotalArticles(articlesOfTopics));
+      } catch (error) {
+        console.error("Failed fetching keyword data.", error);
+      }
+    };
+
+    // TopicsContainer 에서 로드한 실시간 검색어를 먼저 렌더링 후 인터벌로 업데이트.
+    const intervalFetch = setInterval(() => {
+      fetchKeyword();
+    }, 1000 * 60);
+
+    return () => clearInterval(intervalFetch);
+  }, []);
+
   return <></>;
 }
--- app/component/Topics.tsx
+++ app/component/Topics.tsx
@@ -33,66 +33,59 @@ export default function Topics({ data }: TopicsProps): JSX.Element {
   const router = useRouter();
   const { newsOfTopicsList, keywordsData, totalArticles } = data;
 
+  // FetchTopics.tsx 완성 후 삭제 예정.
   useEffect(() => {
     /**
      * fetchKeyword
      * 실시간 검색어를 순회하여 각 검색어에 해당하는 뉴스 리스트 요청.
      * 실시간 검색어를 순회하여 얻은 뉴스 리스트마다 중첩으로 순회하여 각 뉴스 기사 데이터 요청.
      */
-    const fetchKeyword = async () => {
-      try {
-        const topicsResponse = await axios.get("/api/topics");
-        // 실시간 검색어 별로 순회하여 뉴스 리스트 요청 후 배열로 상태 업데이트.
-
-        // 모든 실시간 검색어 키워드에 해당하는 뉴스 리스트들의 각 뉴스 기사 데이터.
-        const articlesOfTopics: { keyword: string; articles: articleData[] }[] = [];
-
-        const results = await Promise.all(
-          topicsResponse.data.top10.map(async (item: TopicsType) => {
-            const response = await axios.post("/api/news", {
-              inputValue: item.keyword,
-              sort: "relation",
-            });
-            const data = {
-              keyword: item.keyword,
-              newsList: response.data.newsList,
-            };
-
-            // 뉴스 리스트를 순회하여 각 기사의 url 수집.
-            const urls: string[] = [];
-            response.data.newsList.forEach((item: newsList) => {
-              if (item.href !== "") {
-                urls.push(item.href);
-              }
-            });
-
-            // 키워드에 해당하는 뉴스 리스트의 각 뉴스 기사 데이터 요청.
-            const responseArticles = await axios.post("/api/articles", { urls: urls });
-            const articlesData = {
-              keyword: item.keyword,
-              articles: responseArticles.data.articlesData,
-            };
-
-            // 위의 뉴스 리스트의 각 뉴스 기사 데이터 (articles) 를 articlesOfTopics 배열에 푸쉬.
-            articlesOfTopics.push(articlesData);
-
-            return data;
-          }),
-        );
-        setNewsListArray(results);
-        setTopics(topicsResponse.data.top10);
-        dispatch(setTotalArticles(articlesOfTopics));
-      } catch (error) {
-        console.error("Failed fetching keyword data.", error);
-      }
-    };
-
-    // TopicsContainer 에서 로드한 실시간 검색어를 먼저 렌더링 후 인터벌로 업데이트.
-    const intervalFetch = setInterval(() => {
-      fetchKeyword();
-    }, 1000 * 60);
-
-    return () => clearInterval(intervalFetch);
+    // const fetchKeyword = async () => {
+    //   try {
+    //     const topicsResponse = await axios.get("/api/topics");
+    //     // 실시간 검색어 별로 순회하여 뉴스 리스트 요청 후 배열로 상태 업데이트.
+    //     // 모든 실시간 검색어 키워드에 해당하는 뉴스 리스트들의 각 뉴스 기사 데이터.
+    //     const articlesOfTopics: { keyword: string; articles: articleData[] }[] = [];
+    //     const results = await Promise.all(
+    //       topicsResponse.data.top10.map(async (item: TopicsType) => {
+    //         const response = await axios.post("/api/news", {
+    //           inputValue: item.keyword,
+    //           sort: "relation",
+    //         });
+    //         const data = {
+    //           keyword: item.keyword,
+    //           newsList: response.data.newsList,
+    //         };
+    //         // 뉴스 리스트를 순회하여 각 기사의 url 수집.
+    //         const urls: string[] = [];
+    //         response.data.newsList.forEach((item: newsList) => {
+    //           if (item.href !== "") {
+    //             urls.push(item.href);
+    //           }
+    //         });
+    //         // 키워드에 해당하는 뉴스 리스트의 각 뉴스 기사 데이터 요청.
+    //         const responseArticles = await axios.post("/api/articles", { urls: urls });
+    //         const articlesData = {
+    //           keyword: item.keyword,
+    //           articles: responseArticles.data.articlesData,
+    //         };
+    //         // 위의 뉴스 리스트의 각 뉴스 기사 데이터 (articles) 를 articlesOfTopics 배열에 푸쉬.
+    //         articlesOfTopics.push(articlesData);
+    //         return data;
+    //       }),
+    //     );
+    //     setNewsListArray(results);
+    //     setTopics(topicsResponse.data.top10);
+    //     dispatch(setTotalArticles(articlesOfTopics));
+    //   } catch (error) {
+    //     console.error("Failed fetching keyword data.", error);
+    //   }
+    // };
+    // // TopicsContainer 에서 로드한 실시간 검색어를 먼저 렌더링 후 인터벌로 업데이트.
+    // const intervalFetch = setInterval(() => {
+    //   fetchKeyword();
+    // }, 1000 * 60);
+    // return () => clearInterval(intervalFetch);
   }, []);
 
   // Link 컴포넌트의 리디렉션 막고 useRouter 로 리디렉션. (리디렉션 전에 상태 업데이트 하기 위함)
