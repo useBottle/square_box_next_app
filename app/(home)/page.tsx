4fbd0b037598dbcfb48792da2959ef49c0fb8e6c
Jeong Byeong Ho <zero8660@gmail.com>
2025-01-12 21:27:34 +0900

    TopicsContainer, LatestNewsContainer 를 Home 컴포넌트로 통합.

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,13 +1,63 @@
-import LatestNewsContainer from "../component/LatestNewsContainer";
-import TopicsContainer from "../component/TopicsContainer";
+import { getLatestNewsList } from "@/app/actions/latestNewsActions";
+import LatestNews from "@/app/component/LatestNews";
+import Topics from "@/app/component/Topics";
+import { TopicsListType, TopicsType } from "@/types/types";
+import axios from "axios";
 
-export const dynamic = "force-dynamic";
+const fetchKeyword = async (): Promise<TopicsType[] | undefined> => {
+  try {
+    const response = await axios.get(process.env.TOPICS_API_URL || "");
+    return response.data.top10;
+  } catch (error) {
+    console.error("Failed fetching keyword data.", error);
+  }
+};
 
-export default function Home(): JSX.Element {
+// 실시간 검색어를 순회하여 각 검색어에 해당하는 뉴스 리스트 요청.
+const fetchNewsOfTopicsList = async (keywords: string[]) => {
+  try {
+    const newsListsResults = await Promise.all(
+      keywords.map(async (keyword: string) => {
+        // 순회중인 키워드의 뉴스 리스트 요청.
+        const response = await axios.post(`${process.env.NEXTAUTH_URL}/api/news` || "", {
+          inputValue: keyword,
+          sort: "relation",
+        });
+        return {
+          keyword: keyword,
+          newsList: response.data.newsList,
+        };
+      }),
+    );
+    return newsListsResults;
+  } catch (error) {
+    console.error("News of topicsw fetch failed.", error);
+  }
+};
+
+const fetchLatestNewsList = async () => {
+  const latestNewsList = await getLatestNewsList();
+
+  return {
+    latestNewsListFromServer: latestNewsList?.newsTop10List,
+  };
+};
+
+export default async function Home() {
+  const keywordsData: TopicsType[] | undefined = await fetchKeyword();
+  const keywords = keywordsData?.map((item) => item.keyword);
+  const newsOfTopicsList: TopicsListType[] | undefined = await fetchNewsOfTopicsList(keywords as string[]);
+  const latestNewsList = await fetchLatestNewsList();
+
+  const dataOfTopics = {
+    keywordsData: keywordsData,
+    newsOfTopicsList: newsOfTopicsList,
+  };
+
   return (
     <div>
-      <TopicsContainer />
-      <LatestNewsContainer />
+      <Topics data={dataOfTopics} />
+      <LatestNews data={latestNewsList} />
     </div>
   );
 }
