0d2156649a08d054aedcd88032c6cd74c5c0f4cc
Jeong Byeong Ho <zero8660@gmail.com>
2025-01-13 18:57:40 +0900

    FetchBookmark 컴포넌트에서 유저 정보에 해당하는 북마크 데이터 가져와 디스패치.

--- app/component/FetchBookmark.tsx
+++ app/component/FetchBookmark.tsx
@@ -2,19 +2,31 @@
 
 import { useSession } from "next-auth/react";
 import { useEffect } from "react";
-import { getMarkedNews } from "../actions/bookmarkActions";
+import { getMarkedNews, getMarkedYoutube } from "../actions/bookmarkActions";
+import { useDispatch } from "react-redux";
+import { AppDispatch } from "@/store/store";
+import { setMarkedNews, setMarkedYoutube } from "@/store/bookmark";
 
 export default function FetchBookmark(): JSX.Element {
   const { data: session } = useSession();
+  const dispatch = useDispatch<AppDispatch>();
 
   useEffect(() => {
     if (!session || !session.user || session.user.name === undefined) return;
 
-    const fetchBookmark = async () => {
+    const fetchBookmarkData = async () => {
       const newsData = await getMarkedNews(session.user.name as string);
+      if (newsData?.exists === true && newsData.number !== 0 && newsData.data !== undefined) {
+        dispatch(setMarkedNews(newsData));
+      }
+
+      const youtubeData = await getMarkedYoutube(session.user.name as string);
+      if (youtubeData?.exists === true && youtubeData.number !== 0 && youtubeData.data !== undefined) {
+        dispatch(setMarkedYoutube(youtubeData));
+      }
     };
 
-    fetchBookmark();
+    fetchBookmarkData();
   }, []);
 
   return <></>;
