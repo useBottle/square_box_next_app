c7de8080f4c2d86b4d0f5b1b2e0bec9f7ebdaaed
Jeong Byeong Ho <zero8660@gmail.com>
2025-01-13 18:07:36 +0900

    에러 바운더리 error.tsx 컴포넌트에서 현재 경로에 따라 Home 버튼 클릭 시 동작 결정.

--- app/error.tsx
+++ app/error.tsx
@@ -4,8 +4,19 @@
 
 import { CSSObject, css } from "@emotion/react";
 import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
+import { MouseEvent } from "react";
 
 export default function Error(): JSX.Element {
+  const path = usePathname();
+  const router = useRouter();
+
+  const onClick = (e: MouseEvent) => {
+    e.preventDefault();
+    path !== "/" && router.push("/");
+    path === "/" && window.location.reload();
+  };
+
   const infoText: CSSObject = {
     display: "flex",
     justifyContent: "center",
@@ -27,7 +38,9 @@ export default function Error(): JSX.Element {
     <div css={css(infoText)}>
       <p>페이지에 오류가 발생했습니다</p>
       <p>다시 시도해주세요</p>
-      <Link href="/">HOME</Link>
+      <Link href="/" onClick={onClick}>
+        HOME
+      </Link>
     </div>
   );
 }
