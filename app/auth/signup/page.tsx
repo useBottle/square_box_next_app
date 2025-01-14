3d9639b1416b05361b5fa6491afebcba33443457
Jeong Byeong Ho <zero8660@gmail.com>
2025-01-14 15:10:14 +0900

    회원가입 양식 조건 수정 및 양식 입력 상황에 따라 알맞은 alert 창 띄우도록 적용.

--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -16,17 +16,23 @@ export default function Signup(): JSX.Element {
   const [password, setPassword] = useState<string>("");
   const router = useRouter();
 
-  // 이메일: 영문 대, 소문자, 숫자로 시작하고 @, . 기호 포함
-  const emailCondition = /^[A-Za-z-0-9\-\.]+@[A-Ja-z-0-9\-\.]+\.[A-Ja-z-0-9]+$/;
-  // 이름: 영문 대, 소문자, 한글 4~20자.
-  const nameCondition = /^[a-zA-Z가-힣]{4,20}$/;
-  // 패스워드: 대문자, 소문자, 숫자, 특수문자 각각 1개 이상을 포함한 8자 이상
-  const passwordCondition = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,}$/;
+  // 이메일: 영문 대, 소문자, 숫자로 시작하고 @, . 기호 포함 + 빈 문자열 허용
+  const emailCondition = /^[A-Za-z-0-9\-\.]+@[A-Ja-z-0-9\-\.]+\.[A-Ja-z-0-9]+$|^$/;
+  // 이름: 영문 대, 소문자, 한글 4~20자 + 빈 문자열 허용
+  const nameCondition = /^[a-zA-Z가-힣]{4,20}$|^$/;
+  // 패스워드: 대문자, 소문자, 숫자, 특수문자 각각 1개 이상을 포함한 8자 이상 + 빈 문자열 허용
+  const passwordCondition = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,}$|^$/;
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     if (email === "" || name === "" || password === "") {
+      alert("이메일, 이름, 패스워드를 모두 입력해야 합니다.");
+      return;
+    }
+
+    if (!emailCondition.test(email) || !nameCondition.test(name) || !passwordCondition.test(password)) {
+      alert("회원가입 양식 조건에 맞지 않습니다");
       return;
     }
 
