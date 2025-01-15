ea767e399ecb3227fdd0cb0e18f4785733d50fcf
Jeong Byeong Ho <zero8660@gmail.com>
2025-01-16 02:28:45 +0900

    youtubeWrapper 적용 후 레이아웃 틀어지는 이슈 해결.

--- styles/Youtube.styles.ts
+++ styles/Youtube.styles.ts
@@ -1,173 +1,177 @@
 import { CSSObject, keyframes } from "@emotion/react";
 
 export const youtube: CSSObject = {
-  display: "flex",
-  justifyContent: "center",
-  alignItems: "center",
-  flexDirection: "column",
-  overflowX: "hidden",
-  width: "100%",
-
-  ".initYoutube": {
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
-    height: "45rem",
-    width: "100%",
-
-    ".icon": {
-      fontSize: "6rem",
-      marginBottom: "7rem",
-    },
-
-    ".textNback": {
-      position: "relative",
-      display: "flex",
-      flexDirection: "column",
-      alignItems: "center",
-      width: "60%",
-
-      h1: {
-        position: "absolute",
-        top: "50%",
-        transform: "translateY(-50%)",
-        display: "flex",
-        justifyContent: "center",
-        width: "100%",
-        fontSize: "1.6rem",
-        background: "var(--reverse-font)",
-        zIndex: 10,
-        border: "1px solid var(--basic-font)",
-        borderLeft: "none",
-        borderRight: "none",
-        padding: "1rem 0",
-      },
-
-      ".backIcon": {
-        fontSize: "23rem",
-        opacity: 0.1,
-      },
-    },
-  },
-
-  ul: {
+  ".youtubeWrapper": {
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
     flexDirection: "column",
+    overflowX: "hidden",
     width: "100%",
-
-    a: {
-      width: "90%",
-      margin: "2rem 0",
-
-      li: {
-        width: "100%",
-
-        img: {
-          width: "100%",
-          height: "auto",
-        },
-
-        ".textGroup": {
-          padding: "1rem 0",
-
-          ".title": {
-            fontSize: "2rem",
-            color: "var(--title-color)",
-            margin: "1rem 0",
-          },
-
-          ".channel": {
-            fontSize: "1.4rem",
-            padding: "0.5rem 1rem",
-            color: "var(--reverse-font)",
-            background: "var(--basic-font)",
-            border: "1px solid var(--basic-font)",
-            borderRadius: "3px",
-            width: "fit-content",
-            margin: "2rem 0",
-          },
-
-          ".publishedAt": {
-            fontSize: "1.2rem",
-            color: "var(--basic-dark)",
-            margin: "2rem 0",
-          },
-
-          ".description": {
-            fontSize: "1.4rem",
-            color: "var(--basic-font)",
-            margin: "1rem 0",
-          },
-        },
-      },
-
-      "&:hover": {
-        background: "var(--list-back)",
-      },
-    },
-  },
-
-  "@media (min-width: 1200px)": {
-    width: "40vw",
-    marginTop: "10rem",
 
     ".initYoutube": {
-      height: "90rem",
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "45rem",
+      width: "100%",
 
       ".icon": {
-        fontSize: "5rem",
-        marginBottom: "15rem",
+        fontSize: "6rem",
+        marginBottom: "7rem",
       },
 
       ".textNback": {
+        position: "relative",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        width: "60%",
+
         h1: {
-          fontSize: "2rem",
-          padding: "1.5rem 0",
+          position: "absolute",
+          top: "50%",
+          transform: "translateY(-50%)",
+          display: "flex",
+          justifyContent: "center",
+          width: "100%",
+          fontSize: "1.6rem",
+          background: "var(--reverse-font)",
+          zIndex: 10,
+          border: "1px solid var(--basic-font)",
+          borderLeft: "none",
+          borderRight: "none",
+          padding: "1rem 0",
         },
 
         ".backIcon": {
-          fontSize: "40rem",
+          fontSize: "23rem",
+          opacity: 0.1,
         },
       },
     },
 
     ul: {
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      flexDirection: "column",
+      width: "100%",
+
       a: {
-        width: "100%",
-        margin: "3rem 0",
+        width: "90%",
+        margin: "2rem 0",
 
         li: {
-          display: "grid",
-          gridTemplateColumns: "30% 70%",
-          alignItems: "center",
-          padding: "1rem",
+          width: "100%",
 
           img: {
             width: "100%",
-            height: "100%",
+            height: "auto",
           },
 
           ".textGroup": {
-            padding: "0 2rem",
-            height: "100%",
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "space-between",
+            padding: "1rem 0",
 
             ".title": {
               fontSize: "2rem",
-              margin: 0,
+              color: "var(--title-color)",
+              margin: "1rem 0",
             },
 
             ".channel": {
-              marginBottom: 0,
+              fontSize: "1.4rem",
+              padding: "0.5rem 1rem",
+              color: "var(--reverse-font)",
+              background: "var(--basic-font)",
+              border: "1px solid var(--basic-font)",
+              borderRadius: "3px",
+              width: "fit-content",
+              margin: "2rem 0",
+            },
+
+            ".publishedAt": {
+              fontSize: "1.2rem",
+              color: "var(--basic-dark)",
+              margin: "2rem 0",
             },
 
             ".description": {
-              margin: 0,
+              fontSize: "1.4rem",
+              color: "var(--basic-font)",
+              margin: "1rem 0",
+            },
+          },
+        },
+
+        "&:hover": {
+          background: "var(--list-back)",
+        },
+      },
+    },
+  },
+
+  "@media (min-width: 1200px)": {
+    ".youtubeWrapper": {
+      width: "40vw",
+      marginTop: "10rem",
+
+      ".initYoutube": {
+        height: "90rem",
+
+        ".icon": {
+          fontSize: "5rem",
+          marginBottom: "15rem",
+        },
+
+        ".textNback": {
+          h1: {
+            fontSize: "2rem",
+            padding: "1.5rem 0",
+          },
+
+          ".backIcon": {
+            fontSize: "40rem",
+          },
+        },
+      },
+
+      ul: {
+        a: {
+          width: "100%",
+          margin: "3rem 0",
+
+          li: {
+            display: "grid",
+            gridTemplateColumns: "30% 70%",
+            alignItems: "center",
+            padding: "1rem",
+
+            img: {
+              width: "100%",
+              height: "100%",
+            },
+
+            ".textGroup": {
+              padding: "0 2rem",
+              height: "100%",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "space-between",
+
+              ".title": {
+                fontSize: "2rem",
+                margin: 0,
+              },
+
+              ".channel": {
+                marginBottom: 0,
+              },
+
+              ".description": {
+                margin: 0,
+              },
             },
           },
         },
